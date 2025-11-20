873cce7743596c8f4f445961cb9fe7696923f3bd
Nubcoder0118 <brad.hu.2006@gmail.com>
2025-11-20 12:35:21 -0500

    Task 1.1

--- src/main/java/theater/Constants.java
+++ src/main/java/theater/Constants.java
@@ -10,10 +10,11 @@ public final class Constants {
     public static final int COMEDY_EXTRA_VOLUME_FACTOR = 5;
     // comedy amount constants
     public static final int COMEDY_AMOUNT_PER_AUDIENCE = 300;
-    public final static int COMEDY_AUDIENCE_THRESHOLD = 20;
+    public static final int COMEDY_AUDIENCE_THRESHOLD = 20;
     public static final int COMEDY_BASE_AMOUNT = 30000;
     public static final int COMEDY_OVER_BASE_CAPACITY_AMOUNT = 10000;
     public static final int COMEDY_OVER_BASE_CAPACITY_PER_PERSON = 500;
+    public static final int CENTS_PER_DOLLAR = 100;
     // tragedy amount constants
     public static final int TRAGEDY_AUDIENCE_THRESHOLD = 30;
     public static final int TRAGEDY_BASE_AMOUNT = 40000;
